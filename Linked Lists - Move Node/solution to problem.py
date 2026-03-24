b60e91c5d0f0f3775bdfe5593ec499e3e3b9d5b2
Sofia <sofia.gavronska@gmail.com>
2026-03-24 22:03:00 +0200

    Comiting the solution to Linked Lists - Move Node problem

---
+++ Linked Lists - Move Node/solution to problem.py
@@ -0,0 +1,18 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+def move_node(source, dest):
+    if source is None:
+        raise ValueError
+    first = source
+    source = source.next
+    first.next = dest
+    dest = first
+    return Context(source, dest)
